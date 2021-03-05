ded430711a936684c498c2d35fda99c7bd32af48
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-05 17:58:29 +0900

    ローカルファイルを選択、再生するコンポーネントの作成

---
+++ src/components/LocalFilePlayer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Uploader @change="setFile" />
+    <p v-show="msgOn">{{ msg }}</p><br />
+  </div>
+
+  <div >
+    <VideoPlayer :videoSrc="videoSrc"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Uploader from "@/components/Uploader.vue";
+import VideoPlayer from "@/components/VideoPlayer.vue";
+
+export default defineComponent({
+  name: "LocalFilePlayer",
+  props: {},
+  components: {
+    Uploader,
+    VideoPlayer,
+  },
+  data() {
+    return {
+      ready: false,
+      msg: "",
+      msgOn: true,
+      videoSrc: "",
+    };
+  },
+  methods: {
+    setFile: function(e) {
+      const file = e.target.files[0];
+      this.ready = true;
+      this.msg = "selected : " + file.name;
+      this.msgOn = true;
+
+      this.videoSrc = window.URL.createObjectURL(file);
+      this.msg = this.videoSrc
+      //this.data.name = file.name;
+      //this.data.type = file.type;
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
