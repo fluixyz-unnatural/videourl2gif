84ffcf6ddae8f4b7d6d77466ad3b5c20bd98df3d
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-05 17:46:48 +0900

    コンポーネント確認用のページを追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/preview">Preview</router-link>
   </div>
   <router-view/>
 </template>
@@ -27,4 +28,5 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
---
+++ src/views/ComponentPreview.vue
@@ -0,0 +1,54 @@
+<template>
+  <div> 
+    <h4>LocalFilePlayer</h4>
+    <LocalFilePlayer />
+    <h4>Example1</h4>
+    <Example1 />
+    <h4>Uploader</h4>
+    <Uploader />
+    <h4>Rectangle</h4>
+    <Rectangle />
+    <h4>Button</h4>
+    <Button label="Sample" />
+    <h4>TextBox</h4>
+    <TextBox />
+    <h4>VideoPlayer</h4>
+    <VideoPlayer videoSrc="/7seg.mp4"/>
+  </div>
+  <br /><br /><br /><br /><br />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Uploader from "@/components/Uploader.vue";
+import Rectangle from "@/components/Rectangle.vue";
+import Button from "@/components/Button.vue";
+import TextBox from "@/components/TextBox.vue";
+import VideoPlayer from "@/components/VideoPlayer.vue";
+import LocalFilePlayer from "@/components/LocalFilePlayer.vue"
+import Example1 from "@/components/Example1.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Uploader,
+    Rectangle,
+    Button,
+    TextBox,
+    VideoPlayer,
+    LocalFilePlayer,
+    Example1,
+  },
+});
+</script>
+
+<style scoped>
+br {
+  padding: 100px;
+}
+h4 {
+  padding: 50px 0px 0px 0px;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
