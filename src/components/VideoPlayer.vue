116c131a593e3633e2ddd4d2d9ff321858e67e78
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-05 17:47:59 +0900

    動画プレイヤーコンポーネントを追加

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+  <video controls controlslist="nofullscreen" :src="videoSrc" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VideoPlayer",
+  props: {
+    videSrc:{
+        type: String,
+        default: ""
+    },
+  },
+})
+</script>
+
+<style scoped>
+video {
+  align: center;
+  width: 720px;
+  border-radius: 5px;
+}
+</style>
