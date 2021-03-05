77e2090776959d41e877389f70188e4219b24864
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-05 17:47:39 +0900

    数値入力用テキストボックスの追加

---
+++ src/components/TextBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <input type="text" class="outline-none" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TextBox",
+  props: {
+    
+  },
+});
+</script>
+
+<style scoped>
+.outline-none {
+  outline: none;
+}
+input { 
+  width:100px;
+  border-radius:10px;
+  padding :10px 10px;
+  font-size: 32px;
+  font-weight:bold;
+  color: gray;
+  box-shadow: 1px 1px 10px gray inset;
+  caret-color:lavender;
+  vertical-align:middle;
+  text-align:center;
+  border:none;
+}
+input:focus{
+  background-color:whitesmoke;
+}
+</style>
