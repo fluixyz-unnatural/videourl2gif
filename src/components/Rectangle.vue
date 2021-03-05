8815ac561ed20b1a50cb9bc3c017164e020d0480
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-05 17:47:27 +0900

    svg描画用コンポーネントの下書き

---
+++ src/components/Rectangle.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg viewbox="0 0 300 300" width="300" height="300">
+    <line x1="100" y1="100" x2="200" y2="200" stroke="black"></line>
+    <rect :x="x" y="0" :width="width" :height="height" fill="#cccccc" stroke="black"></rect>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Rectangle",
+  props: {
+    msg: String,
+  },
+  data(){
+    return{ 
+      x:0,
+      height:300,
+      width:300
+    }
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
