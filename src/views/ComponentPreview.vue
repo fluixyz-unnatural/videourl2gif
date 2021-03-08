e1ecb45c7534bdf8a36e50dde2cade47bce409b4
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-08 15:09:34 +0900

    トグルボタンを追加

--- src/views/ComponentPreview.vue
+++ src/views/ComponentPreview.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
+    <h4>ToggleButton</h4>
+    <ToggleButton />
     <h4>Ffmpeg</h4>
-    <Ffmpeg />
+    <Ffmpeg
+      src="https://r6---sn-oguesnz7.googlevideo.com/videoplayback?expire=1615013719&ei=99JCYK2MD4j04gKKx6LICA&ip=217.178.28.118&id=o-AMlLRW5m3nUl1MJLBe9Zuno39ijmWy4Cbi0CTCBPpPbs&itag=299&aitags=133%2C134%2C135%2C136%2C160%2C242%2C243%2C244%2C247%2C278%2C298%2C299%2C302%2C303&source=youtube&requiressl=yes&mh=g2&mm=31%2C26&mn=sn-oguesnz7%2Csn-npoe7n7z&ms=au%2Conr&mv=m&mvi=6&pl=18&initcwndbps=2115000&vprv=1&mime=video%2Fmp4&ns=1gocJM3JuuuqbaVQ8raEiRgF&gir=yes&clen=6338078659&dur=12438.000&lmt=1614966367055391&mt=1614991957&fvip=6&keepalive=yes&fexp=24007246&c=WEB&txp=7316222&n=XIUOa2exKoyHPYUmx0&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgT4iQm6Pbo6Y8ZBvj0l571V4C7WSJWcjh9LbuGN-AjFoCIQCNhlkJ5hRjHIDvWJSkuK3GOHZXijSAC9Zt-jXvFjsu_w%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIga6KxI4w2d_4sJOe2S8YUiBs7r160QVO1OtUs1tQRPh0CIDX2ygPg3jWRafc40Nw5yuNNpPe4SKBIe-NFbfXRhjp1&ratebypass=yes"
+    />
     <h4>LocalFilePlayer</h4>
     <LocalFilePlayer />
     <h4>Uploader</h4>
@@ -37,6 +41,7 @@ import VideoPlayer from "@/components/VideoPlayer.vue";
 import LocalFilePlayer from "@/components/LocalFilePlayer.vue";
 import DrawRect from "@/components/DrawRect.vue";
 import Ffmpeg from "@/components/Ffmpeg.vue";
+import ToggleButton from "@/components/ToggleButton.vue";
 
 export default defineComponent({
   name: "Home",
@@ -49,6 +54,7 @@ export default defineComponent({
     LocalFilePlayer,
     DrawRect,
     Ffmpeg,
+    ToggleButton,
   },
 });
 </script>
