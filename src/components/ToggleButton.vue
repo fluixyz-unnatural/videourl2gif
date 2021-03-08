d480dab5d704f4518e2ed1daa5ba08fc0c2bf531
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-08 15:07:35 +0900

    トグルボタンを追加

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button @click="pushed" :class="{pushed : isPushed,unpushed:!isPushed}">
+    {{ label }} 
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    label: {
+      type: String,
+      default: "ボタン",
+    },
+  },
+  data() {
+    return{ 
+      isPushed : false,
+    }
+  },
+  methods: {
+    pushed: function(){
+      this.isPushed = !this.isPushed
+    }
+  }
+});
+</script>
+
+<style scoped>
+button {
+  outline: none;
+}
+.unpushed {
+  color: #222222;
+  padding: 20px;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 10px;
+  box-shadow: 1px 1px 10px gray;
+  border:none;
+}
+.pushed {
+  color: #222222;
+  padding: 20px;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 10px;
+  box-shadow: 1px 1px 10px gray inset;
+  border:none;
+}
+
+</style>
