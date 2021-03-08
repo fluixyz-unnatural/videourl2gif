c799e276f13bcf68be6afc0a8260a82fb5c2fb5b
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-08 15:08:06 +0900

    再生地点を適当な感覚でemitするように

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,5 +1,10 @@
 <template>
-  <video controls controlslist="nofullscreen" :src="videoSrc" />
+  <video
+    controls
+    controlslist="nofullscreen"
+    :src="videoSrc"
+    @progress="setTimeEmit"
+  />
 </template>
 
 <script lang="ts">
@@ -8,18 +13,36 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "VideoPlayer",
   props: {
-    videoSrc:{
-        type: String,
-        default: ""
+    videoSrc: {
+      type: String,
+      default: "",
     },
   },
-})
+  data() {
+    return {
+      ct: 0,
+    };
+  },
+  methods: {
+    setTimeEmit: function(e: any) {
+      setInterval(() => {
+        {
+          if (e.target.currentTime !== null) {
+            this.$emit("timeemit", e.target.currentTime);
+            this.ct = e.target.currentTime;
+          }
+        }
+      }, 100);
+    },
+  },
+});
 </script>
 
 <style scoped>
 video {
   align: center;
   width: 720px;
+  height: 405px;
   border-radius: 5px;
 }
 </style>
