9bcc2d5b20ee17da8769b54bd3b26afd5cbfe96d
fluixyz-unnatural <sudo3949@gmail.com>
2021-03-09 21:47:06 +0900

    目盛りを追加

--- src/components/Scroller.vue
+++ src/components/Scroller.vue
@@ -36,6 +36,22 @@
         stroke-width="5"
         stroke="gray"
       ></line>
+      <line
+        :x1="0"
+        :x2="0"
+        y1="0"
+        y2="100"
+        :stroke-width="2 * reRatio"
+        stroke="gray"
+      ></line>
+      <line
+        :x1="duration"
+        :x2="duration"
+        y1="0"
+        y2="100"
+        :stroke-width="2 * reRatio"
+        stroke="gray"
+      ></line>
 
       <!-- 選択範囲 !-->
       <line
@@ -54,7 +70,7 @@
         y1="0"
         y2="100"
         :stroke-width="reRatio"
-        stroke="black"
+        stroke="red"
       ></line>
       <!-- start !-->
       <line
@@ -74,8 +90,35 @@
         stroke="dodgerblue"
       ></line>
     </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="720"
+      height="100"
+      class="absolute timeline"
+      preserveAspectRatio="none"
+    >
+      <g v-for="scale in scales" :key="scale">
+        <line
+          v-show="ratio > scale.wipe"
+          v-for="i in 1000"
+          :key="i"
+          :x1="
+            (-viewBoxX % scale.interval) * ratio +
+              scale.interval * (i - 3) * ratio
+          "
+          :x2="
+            (-viewBoxX % scale.interval) * ratio +
+              scale.interval * (i - 3) * ratio
+          "
+          y1="0"
+          :y2="scale.peak < ratio ? 10 : scale.half < ratio ? 5 : 3"
+          :stroke-width="scale.peak < ratio ? 3 : scale.half < ratio ? 2 : 1"
+          stroke="#2c2c2c"
+        ></line>
+      </g>
+    </svg>
   </div>
-  {{ start }}
+  {{ ratio }}
 </template>
 
 <script lang="ts">
@@ -99,7 +142,7 @@ export default defineComponent({
     },
     end: {
       type: Number,
-      default: 10,
+      default: 0,
     },
   },
   data() {
@@ -108,14 +151,38 @@ export default defineComponent({
       timeline: 0,
       viewBoxX: -10,
       isPushed: false,
+      scales: [
+        { interval: 1, wipe: 3, half: 8, peak: 15 },
+        { interval: 5, wipe: 0.8, half: 3, peak: 8 },
+        { interval: 15, wipe: 0.5, half: 0.8, peak: 3 },
+        { interval: 60, wipe: 0.1, half: 0.5, peak: 0.8 },
+        { interval: 60 * 5, wipe: 0, half: 0.5, peak: 1 },
+        { interval: 60 * 15, wipe: 0, half: 0, peak: 1 },
+        { interval: 60 * 60, wipe: 0, half: 0, peak: 0 },
+      ],
     };
   },
   methods: {
     zoom: function(e: any) {
-      this.ratio *= 1 - e.deltaY / 300;
+      let targetR = this.ratio;
+      if (e.deltaY < 0) {
+        targetR *= 1.1;
+      } else {
+        targetR /= 1.1;
+      }
+      targetR = Math.max(0.001, targetR);
+      targetR = Math.min(100, targetR);
+      this.ratio = targetR;
+      let targetX = this.viewBoxX;
+      targetX = Math.min(targetX, this.$props.duration);
+      targetX = Math.max(targetX, -720 / this.ratio);
+      this.viewBoX = targetX;
     },
     slide: function(e: any) {
-      this.viewBoxX += e.deltaY / (10 * this.ratio);
+      let targetX = this.viewBoxX - e.deltaY / (10 * this.ratio);
+      targetX = Math.min(targetX, this.$props.duration);
+      targetX = Math.max(targetX, -720 / this.ratio);
+      this.viewBoxX = targetX;
     },
     setCurrentTime: function(e: any) {
       this.isPushed = true;
